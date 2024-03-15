d7ce17e54ba9927b69605b5f6fb63e09bb72f052
Vishwa <vishwa.m2204@gmail.com>
2024-03-15 11:06:29 +0530

    commit

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+
+// NAvbar BG 
+
+let navbarBg = document.querySelector('.navbar-main');
+
+window.addEventListener('scroll', () => {
+
+    if (window.scrollY >= 300) {
+        navbarBg.classList.add('nav-background');
+    } else if (window.scrollY < 300) {
+        navbarBg.classList.remove('nav-background');
+
+    }
+
+});
